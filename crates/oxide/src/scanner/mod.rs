32dbf84c07167bcfd2b840c81c326a8f740a15a2
Hugo Parente Lima <hugo.pl@gmail.com>
2025-05-02 17:26:07 -0300

    Let slang templates use slim processor.

--- crates/oxide/src/scanner/mod.rs
+++ crates/oxide/src/scanner/mod.rs
@@ -417,7 +417,7 @@ pub fn pre_process_input(content: &[u8], extension: &str) -> Vec<u8> {
         "json" => Json.process(content),
         "pug" => Pug.process(content),
         "rb" | "erb" => Ruby.process(content),
-        "slim" => Slim.process(content),
+        "slim" | "slang" => Slim.process(content),
         "svelte" => Svelte.process(content),
         "vue" => Vue.process(content),
         _ => content.to_vec(),
